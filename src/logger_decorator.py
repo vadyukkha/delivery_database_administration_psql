cbef07aabf691e10f8f5cdea567122dd6972f461
vadim <144333283+vadyukkha@users.noreply.github.com>
2024-11-11 22:11:48 +0300

    first commit

---
+++ src/logger_decorator.py
@@ -0,0 +1,33 @@
+import logging
+import psycopg2
+import os
+from tkinter import messagebox
+from functools import wraps
+from dotenv import load_dotenv
+
+load_dotenv()
+log_file = os.getenv('LOG_PATH', 'logging_data/db_connection.log')
+
+if os.path.exists(log_file):
+    os.remove(log_file)
+
+logging.basicConfig(
+    filename=log_file,
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def dbconnect_logger(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        logging.info(f"Running {func.__name__} with args: {args}, kwargs: {kwargs}")
+        try:
+            result = func(*args, **kwargs)
+            logging.info(f"Finished {func.__name__} with result: {result}")
+            return result
+        except psycopg2.Error as e:
+            logging.error(f"Error in {func.__name__}: {e}")
+            messagebox.showerror("Database Error", f"An error occurred: {e}")
+            return None
+    return wrapper
+
---
+++ src/main.py
@@ -0,0 +1,118 @@
+import os
+import psycopg2
+import tkinter as tk
+from tkinter import ttk, messagebox
+from dotenv import load_dotenv
+from logger_decorator import dbconnect_logger, logging
+
+load_dotenv()
+
+DB_INIT_SCRIPT = os.getenv('DB_INIT_SCRIPT', 'demo_small.sql')
+DB_NAME = os.getenv('DB_NAME', 'airport')
+DB_HOST = os.getenv('DB_HOST', 'localhost')
+DB_PORT = os.getenv('DB_PORT', '5432')
+
+class DispatcherBD:
+    def __init__(self) -> None:
+        self.root = tk.Tk()
+        self.root.title("Login to Database")
+        self.root.geometry("400x400")
+        
+        self.connection = None
+        
+        self.create_login_window()
+
+    def create_login_window(self):
+        ttk.Label(self.root, text="Login:").pack(pady=(20, 5))
+        self.login_entry = ttk.Entry(self.root)
+        self.login_entry.pack(pady=5)
+
+        ttk.Label(self.root, text="Password:").pack(pady=5)
+        self.password_entry = ttk.Entry(self.root, show="*")
+        self.password_entry.pack(pady=5)
+
+        login_button = ttk.Button(self.root, text="Login", command=self.authenticate)
+        login_button.pack(pady=20)
+
+    def authenticate(self):
+        login = self.login_entry.get()
+        password = self.password_entry.get()
+
+        if login == "" or password == "":
+            messagebox.showwarning("Input Error", "Please enter both login and password.")
+            return
+        
+        if self.connect_to_db(login, password):
+            messagebox.showinfo("Login Success", "Successfully logged in!")
+            self.root.destroy()
+            self.show_main_window()
+        else:
+            messagebox.showerror("Login Failed", "Failed to connect to the database with the provided credentials.")
+
+    @dbconnect_logger
+    def connect_to_db(self, login, password):
+        if not self.database_exists():
+            self.initialize_database(login, password)
+
+        self.connection = psycopg2.connect(
+            dbname=DB_NAME,
+            user=login,
+            password=password,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+        return True
+
+    @dbconnect_logger
+    def database_exists(self):
+        with psycopg2.connect(dbname="postgres", user="postgres", host=DB_HOST, port=DB_PORT) as conn:
+            conn.autocommit = True
+            cursor = conn.cursor()
+            cursor.execute(f"SELECT 1 FROM pg_database WHERE datname = '{DB_NAME}'")
+            return cursor.fetchone() is not None
+            
+
+    @dbconnect_logger
+    def initialize_database(self, login, password):
+        connect_admin = psycopg2.connect(
+            dbname="postgres", 
+            user="postgres", 
+            host=DB_HOST, 
+            port=DB_PORT
+        )
+        connect_admin.set_session(autocommit=True)
+        cursor = connect_admin.cursor()
+        cursor.execute(f"CREATE DATABASE {DB_NAME}")
+        logging.info(f"Database '{DB_NAME}' created successfully.")
+
+        # fix not exist role
+        with psycopg2.connect(
+            dbname=DB_NAME,
+            user=login,
+            password=password,
+            host=DB_HOST,
+            port=DB_PORT
+        ) as conn:
+            conn.autocommit = True
+            with open(DB_INIT_SCRIPT, "r") as f:
+                sql_script = f.read()
+                cursor = conn.cursor()
+                cursor.execute(sql_script)
+                logging.info("Database initialized successfully.")
+
+    def show_main_window(self):
+        main_window = tk.Tk()
+        main_window.title("Main Application Window")
+        main_window.geometry("400x400")
+        
+        ttk.Label(main_window, text="Connected to Database!").pack(pady=20)
+
+        # TO-DO: Добавить интерфейс для работы с базой данных
+
+        main_window.mainloop()
+
+    def run(self) -> None:
+        self.root.mainloop()
+
+if __name__ == '__main__':
+    DispatcherBD().run()
